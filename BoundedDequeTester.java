095dbd8d79234a7eac80b1139f2dfa4567748020
Christopher <cnies@ucsd.edu>
2014-04-23 02:10:14 -0700

    Added some tests

--- BoundedDequeTester.java
+++ BoundedDequeTester.java
@@ -2,40 +2,65 @@ import junit.framework.TestCase;
 
 
 public class BoundedDequeTester extends TestCase {
- private Deque12<Integer> empty;
- private Deque12<Integer> one;
- private Deque12<Integer> several;
- private Deque12<Integer> fullMinusOne;
- private Deque12<Integer> full;
- static final int SIZE = 15;
- 
+	private Deque12<Integer> empty;
+	private Deque12<Integer> one;
+	private Deque12<Integer> several;
+	private Deque12<Integer> fullMinusOne;
+	private Deque12<Integer> full;
+	static final int SIZE = 15;
 
- public BoundedDequeTester() {
-  super();
- }
- 
- protected void setUp() throws Exception {
-  super.setUp();
-  empty = new Deque12<Integer>(SIZE);
-  one = new Deque12<Integer>(SIZE);
-  several = new Deque12<Integer>(SIZE);
-  fullMinusOne = new Deque12<Integer>(SIZE);
-  full = new Deque12<Integer>(SIZE);
-  one.addBack(new Integer(1));
-  for (int i =0; i<6; i++)
-   several.addBack(i+1);
-  for (int i=0; i<SIZE-1; i++)
-   fullMinusOne.addBack(i);
-  for (int i = 0; i<SIZE; i++)
-   full.addFront(i);
- }
- 
- public void testAddBack(){
-  assertEquals(new Integer(1), one.peekBack());
- }
 
- protected void tearDown() throws Exception {
-  super.tearDown();
- }
+	public BoundedDequeTester() {
+		super();
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		empty = new Deque12<Integer>(SIZE);
+		one = new Deque12<Integer>(SIZE);
+		several = new Deque12<Integer>(SIZE);
+		fullMinusOne = new Deque12<Integer>(SIZE);
+		full = new Deque12<Integer>(SIZE);
+		one.addBack(new Integer(1));
+		for (int i =0; i<6; i++)
+			several.addBack(new Integer(i+1));
+		for (int i=0; i<SIZE-1; i++)
+			fullMinusOne.addBack(new Integer(i));
+		for (int i = 0; i<SIZE; i++)
+			full.addBack(new Integer(i));
+	}
+
+	//Tests that addBack in the setup worked
+	public void testAddBack(){
+		assertEquals(new Integer(1), one.peekBack());
+		assertEquals(new Integer(6), several.peekBack());
+		assertEquals(new Integer(13), fullMinusOne.peekBack());
+		assertEquals(new Integer(14), full.peekBack());
+	}
+
+	//Make sure addBack returns false on a full
+	public void testAddBackOnFull(){
+		assertTrue(!full.addBack(new Integer(4)));
+	}
+
+	//Makes sure addBack changes size appropriately
+	public void testAddBackSize(){
+		one.addBack(4);
+		empty.addBack(1);
+		assertEquals(1, empty.size());
+		assertEquals(2, one.size()); 
+	}
+
+	//Tests the removeBack method in a simple case
+	public void testRemoveBack(){
+		assertEquals(null, empty.removeBack());
+		assertTrue(0 == empty.size());
+		assertTrue(1 == one.removeBack());
+		assertTrue(0 == one.size());
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
 
 }
--- Deque12.java
+++ Deque12.java
@@ -1,68 +1,72 @@
 import java.util.*;
 
 public class Deque12<E> implements BoundedDeque<E>{
- private int size;
- private int capacity;
- private int front;
- private int back;
- private ArrayList<E> elementArray;
- 
- public Deque12(int capacityIn){
-  elementArray = new ArrayList<E>();
-  for (int i = 0; i<=capacityIn; i++){
-   elementArray.add(null);
-  }
-  this.size = 0;
-  this.capacity = capacityIn;
-  this.front = 0;
-  this.back = 0;
- }
+	private int size;
+	private int capacity;
+	private int front;
+	private int back;
+	private ArrayList<E> elementArray;
 
- public boolean addBack(E element) throws NullPointerException{
-  if (element == null)
-   throw new NullPointerException();
-  if (size == capacity)
-   return false;
-  if (elementArray.get(back)==null){
-   elementArray.set(back, element);
-   size++;
-   return true;
-  }
-  back = (back+1)%elementArray.size();
-  elementArray.set(back, element);
-  size++;
-  return true;
- }
+	public Deque12(int capacityIn){
+		elementArray = new ArrayList<E>();
+		for (int i = 0; i<=capacityIn; i++){
+			elementArray.add(null);
+		}
+		this.size = 0;
+		this.capacity = capacityIn;
+		this.front = 0;
+		this.back = 0;
+	}
 
- public boolean addFront(E element) {
-  // TODO Auto-generated method stub
-  return false;
- }
+	public boolean addBack(E element) throws NullPointerException{
+		if (element == null)
+			throw new NullPointerException();
+		if (size == capacity)
+			return false;
+		if (elementArray.get(back)==null){
+			elementArray.set(back, element);
+			size++;
+			return true;
+		}
+		back = (back+1)%elementArray.size();
+		elementArray.set(back, element);
+		size++;
+		return true;
+	}
 
- public int capacity() {
-  return capacity;
- }
+	public boolean addFront(E element) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
- public E peekBack() {
-  return elementArray.get(back);
- }
+	public int capacity() {
+		return capacity;
+	}
 
- public E peekFront() {
-  // TODO Auto-generated method stub
-  return null;
- }
+	public E peekBack() {
+		return elementArray.get(back);
+	}
 
- public E removeBack() {
-  return null;
- }
+	public E peekFront() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
- public E removeFront() {
-  // TODO Auto-generated method stub
-  return null;
- }
+	public E removeBack() {
+		E toRemove = elementArray.get(back);
+		elementArray.set(back, null);
+		back = (back-1)%elementArray.size();
+		if (size>0) size--;
+		return toRemove;
+	}
 
- public int size() {
-  return size;
- }
+	public E removeFront() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int size() {
+		return size;
+	}
 
 }
